67167dcb40753fdb0da38229da1270c934b01b83
Carl Aischmann <aischmann@cosmocode.de>
2019-09-06 16:37:11 +0200

    YAD-3 Add datetimepicker 'flatpickr'

---
+++ public/js/custom.js
@@ -0,0 +1,3 @@
+flatpickr('[data-calender-type="date"]', {'dateFormat' : 'd.m.Y', 'allowInput' : true});
+flatpickr('[data-calender-type="time"]', {'noCalendar' : true, 'enableTime' : true, 'time_24hr' : true, 'allowInput' : true});
+flatpickr('[data-calender-type="datetime"]', {'enableTime' : true, 'time_24hr' : true, 'dateFormat' : 'd.m.Y H:i', 'allowInput' : true});
