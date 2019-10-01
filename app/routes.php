6caee5a7d35dccf2775e0b791ef236aaa6665f34
Anna Dabrowska <dabrowska@cosmocode.de>
2019-10-01 12:52:36 +0200

    Simplify routes

--- app/routes.php
+++ app/routes.php
@@ -3,23 +3,10 @@ declare(strict_types=1);
 
 use CosmoCode\Formserver\Actions\DownloadAction;
 use CosmoCode\Formserver\Actions\FormAction;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
-use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $container = $app->getContainer();
-
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
-    });
-
-    $app->group('/forms', function (Group $group) use ($container) {
-        $group->get('', FormAction::class);
-        $group->any('/{id}', FormAction::class);
-    });
-
     $app->get('/download/{id}', DownloadAction::class);
+    $app->any('/{id}', FormAction::class);
+    $app->get('/', FormAction::class);
 };
