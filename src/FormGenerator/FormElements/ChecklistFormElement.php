967a681dce0b45c83f26b6c15d6bc2c36b60b7fd
Carl Aischmann <aischmann@cosmocode.de>
2019-12-10 14:30:03 +0100

    YAD-21 Enable markdown in checkboxlist labels (breaking change)

--- src/FormGenerator/FormElements/ChecklistFormElement.php
+++ src/FormGenerator/FormElements/ChecklistFormElement.php
@@ -2,10 +2,42 @@
 
 namespace CosmoCode\Formserver\FormGenerator\FormElements;
 
+use Michelf\MarkdownExtra;
+
 /**
  * Representation of a checkbox group
  */
 class ChecklistFormElement extends AbstractDynamicFormElement
 {
+    /**
+     * Override parent to transform markdown choice labels
+     *
+     * @return array
+     */
+    public function getViewVariables()
+    {
+        $choices = $this->getConfigValue('choices');
+        $transformedChoices = [];
 
+        foreach ($choices as $choiceValue => $choiceLabel) {
+            $transformedChoiceLabel = MarkdownExtra::defaultTransform($choiceLabel);
+
+            // Markdown lib always wraps the content in a <p>...</p>
+            // https://github.com/michelf/php-markdown/issues/230
+            $transformedChoiceLabel = str_replace(
+                ['<p>', '</p>'],
+                '',
+                $transformedChoiceLabel
+            );
+
+            $transformedChoices[$transformedChoiceLabel] = $choiceValue;
+        };
+
+        return array_merge(
+            parent::getViewVariables(),
+            [
+                'choices' => $transformedChoices
+            ]
+        );
+    }
 }
