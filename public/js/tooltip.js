a1de75378783261cd4c3037a450b0420dbbe1614
Anna Dabrowska <dabrowska@cosmocode.de>
2024-08-29 16:15:43 +0200

    Minor style changes

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -5,7 +5,7 @@ export function tooltips() {
     Array.from(document.querySelectorAll('.with-tooltip .control button.tooltip-button')).forEach(function(elem) {
         const prev = elem.previousElementSibling;
         const headlines = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
-        if (headlines.includes(prev.tagName)) {
+        if (prev && headlines.includes(prev.tagName)) {
             prev.appendChild(elem);
         }
     });
