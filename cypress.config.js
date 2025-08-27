cc7e7c683b5f1df0d0a20b66532b40bae8c4b391
Anna Dabrowska <dabrowska@cosmocode.de>
2025-08-27 18:31:00 +0200

    Test updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: process.env.CYPRESS_BASE_URL,
         setupNodeEvents(on, config) {
             return config;
         },
+        baseUrl: process.env.CYPRESS_BASE_URL,
     },
 });
