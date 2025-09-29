d76cce91d9f62b3d8296953b21c7f01a0bd19504
Anna Dabrowska <dabrowska@cosmocode.de>
2025-09-29 11:19:13 +0200

    Test form meta

--- cypress/e2e/EXAMPLE.cy.js
+++ cypress/e2e/EXAMPLE.cy.js
@@ -19,6 +19,27 @@ describe('Test EXAMPLE config', () => {
         cy.clearOPFS();
     });
 
+    it('form meta: title', () => {
+        cy.title().should('eq', 'Form example');
+    });
+
+    it('form meta: logo', () => {
+        cy.get('img.logo')
+            .should('be.visible')
+            .and(($img) => {
+                expect($img[0].naturalWidth).to.be.greaterThan(0);
+            });
+    });
+
+    it('form meta: custom CSS', () => {
+        cy.get("html")
+            .should(
+                'have.css',
+                'background-color',
+                'rgb(213, 245, 255)' // RGB for HEX from custom.css #d5f5ff
+            );
+    });
+
     it('"static fieldset" link is clicked and its label becomes visible', () => {
         cy.contains('Static elements').click();
         cy.get('#fieldset0\\.fieldset_static > div.label').should('be.visible');
